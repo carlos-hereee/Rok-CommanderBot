107d20f9d52a523fe69c5242ac3fbf856add4550
carlos-hereee <97hernandez.c@gmail.com>
2024-09-05 09:22:30 -0500

    run build

--- src/commands/rok/castle.ts
+++ src/commands/rok/castle.ts
@@ -1,16 +1,16 @@
-import { formatEmbedCastle, errorEmbed, sendEmbed }  from "../embed";
-import { getCastle }  from "@db/modal/getCastle";
-import { notFound }  from "@db/error.json";
+import { formatEmbedCastle, errorEmbed, sendEmbed } from "../embed";
+import { getCastle } from "@db/modal/getCastle";
+import { notFound } from "@db/error.json";
 
-export= {
-  name: "Castle Upgrades",
-  description: "Learn information about castle upgrades",
-  triggers: ["castle"],
-  handler: async (message) => {
-    const msg = message.content.split(" ").pop();
-    const level = await getCastle(parseInt(msg,10));
-    if (level.length === 0) return sendEmbed(message, notFound)
-      const options = formatEmbedCastle(level.pop())
-    return sendEmbed(message, options)
-};
+export = {
+	name: "Castle Upgrades",
+	description: "Learn information about castle upgrades",
+	triggers: ["castle"],
+	handler: async (message) => {
+		const msg = message.content.split(" ").pop();
+		const level = await getCastle(parseInt(msg, 10));
+		if (level.length === 0) return sendEmbed(message, notFound);
+		const options = formatEmbedCastle(level.pop());
+		return sendEmbed(message, options);
+	},
 };
